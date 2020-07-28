c8ef691fbfc744c4e6c060cc9ac6e0477c4cc39f
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-28 04:13:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase == 
+  if phrase == "I LOVE YOU GRANDMA!"
+    return 
     elsif phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase 
