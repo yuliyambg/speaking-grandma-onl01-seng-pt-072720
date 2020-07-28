62ff12551bb13903db078805a5736ef1b9c1fcf4
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-28 04:09:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,7 @@ def speak_to_grandma(string)
   if string == string.downcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
+  return 
 end
 
 # Whatever you say to grandma, she should respond with
