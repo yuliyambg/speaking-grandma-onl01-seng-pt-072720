53be153ad268936b121b3d89ade0bc4a05414e67
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-28 04:24:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.downcase
+    elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else phrase == phrase.upcase 
   return "NO, NOT SINCE 1938!"
