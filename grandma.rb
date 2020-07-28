3131349a1f0a7374ad13e7dc9bcb2b9a5ce8a3f4
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-28 04:07:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
-  if phrase == "phrase".downcase
+def speak_to_grandma(string)
+  if string == "phrase".downcase
     return "HUH?! SPEAK UP, SONNY!"
 end
 
